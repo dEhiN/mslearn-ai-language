eda3c5284446e9f7f04f640cd10ddcbee9419dc8
David <hiranwatson@gmail.com>
2023-12-29 18:32:19 -0500

    Finished last sections

--- Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
+++ Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
@@ -104,6 +104,19 @@ def main():
 
             # Get linked entities
 
+            entities = ai_client.recognize_linked_entities(documents=[text])[
+                0
+            ].entities
+
+            if len(entities) > 0:
+                print("\nLinks:")
+                for linked_entity in entities:
+                    print(
+                        "\t{} ({})".format(
+                            linked_entity.name, linked_entity.url
+                        )
+                    )
+
     except Exception as ex:
         print(ex)
 
